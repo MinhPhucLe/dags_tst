8f88b8f8d9901ba00ad1e80c7280f7d0e880c43d
MinhPhucLe <leminhphucwork@gmail.com>
2025-04-27 18:51:51 +0700

    tst send mail

--- dags/tst_dag.py
+++ dags/tst_dag.py
@@ -3,27 +3,46 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.utils.email import send_email
 import logging
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
 
 start_date = datetime(2024, 12, 15)
 
 
-# def on_success_callback(context):
-#     # Extract information from the context
-#     task_instance = context['task_instance']
-#     execution_date = context['execution_date']
-#
-#     # Format a message without the exception
-#     message = f"Task {task_instance.task_id} was successful on {execution_date}"
-#
-#     logging.info(f"Sending success email: {message}")
-#
-#     # Send an email
-#     try:
-#         send_email(to='tryrequestamin123@gmail.com', subject='Task Success', html_content=message)
-#         logging.info("Success email sent.")
-#     except Exception as e:
-#         logging.error(f"Failed to send success email: {e}")
-#         raise
+def send_email_via_smtp(subject, body, to_email):
+    from_email = "tryrequestamin123@gmail.com"
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_user = "tryrequestamin123@gmail.com"
+    smtp_password = "sncg rsxl oyuf cgzn"
+
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['To'] = to_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    with smtplib.SMTP(smtp_server, smtp_port) as server:
+        server.starttls()
+        server.login(smtp_user, smtp_password)
+        server.sendmail(from_email, to_email, msg.as_string())
+
+# Hàm gửi email khi thành công
+def send_success_email(context):
+    subject = f"DAG Success: {context['dag'].dag_id}"
+    body = f"The DAG {context['dag'].dag_id} has successfully completed."
+    send_email_via_smtp(subject, body, "tryrequestamin123@gmail.com")
+
+# Hàm gửi email khi thất bại
+def send_failure_email(context):
+    subject = f"DAG Failed: {context['dag'].dag_id}"
+    body = f"The DAG {context['dag'].dag_id} has failed."
+    send_email_via_smtp(subject, body, "tryrequestamin123@gmail.com")
 
 def tst():
     a = 3 + 4
